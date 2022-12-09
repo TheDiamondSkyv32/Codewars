700c8654f8e1e5ca3d098c6075ca887e2a6b5ac3
TheDiamondSkyv32 <dg546@njit.edu>
2022-12-08 20:39:16 -0500

    given int array sorted in non-decreasing order, return array of squares of each number sorted in non-decreasing order

---
+++ LeetCodeDS/SquaresSortedArray.py
@@ -0,0 +1,33 @@
+"""
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+"""
+
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        squaredArray = [k**2 for k in nums] # create new list of squared integers
+        squaredArray.sort() # sort the numbers using built-in sort (Timsort)
+        return squaredArray 
